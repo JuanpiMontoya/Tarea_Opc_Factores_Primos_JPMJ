c87c28e2d648c8ee5ec95ec90b69cd6388631d3b
Juan Pablo Montoya <juan.montoya@ucb.edu.bo>
2023-09-23 11:10:33 -0400

    Factores Primos - caso del número 6

--- src/Factores_Primos.test.js
+++ src/Factores_Primos.test.js
@@ -14,8 +14,11 @@ describe("Factores Primos", () => {
   });
 
   it("La lista devuelve los dos factores al escribir 4", () => {
-    console.log(mostrarPrimos(4));
     expect(mostrarPrimos(4)).toEqual([2,2]);
+  });
+
+  it("La lista devuelve los dos factores al escribir 6", () => {
+    expect(mostrarPrimos(6)).toEqual([2,3]);
   });
 
 });
