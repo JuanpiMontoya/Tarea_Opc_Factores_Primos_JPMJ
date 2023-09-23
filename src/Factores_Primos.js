be39bdb0a129d34e0dfe97240bee81983f5cecd3
Juan Pablo Montoya <juan.montoya@ucb.edu.bo>
2023-09-23 10:33:28 -0400

    Factores Primos - caso del número 2

--- src/Factores_Primos.js
+++ src/Factores_Primos.js
@@ -1,7 +1,11 @@
 
 function mostrarPrimos(num)
 {
-    return []
+    const Fac_Primos = []
+    if (num > 1) {
+        Fac_Primos.push(2);
+    }
+    return Fac_Primos;
 }
 
 export default mostrarPrimos;
--- src/Factores_Primos.test.js
+++ src/Factores_Primos.test.js
@@ -4,4 +4,8 @@ describe("Factores Primos", () => {
   it("Se debe devolver una lista vacia al ingresar 1", () => {
     expect(mostrarPrimos(1)).toEqual([]);
   });
+
+  it("La lista devuelve los factores al escribir 2", () => {
+    expect(mostrarPrimos(2)).toEqual([2]);
+  });
 });
